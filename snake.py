d969332bd3cf6eddc3f84ff3c0c0edb567d194d4
Juan Puelpan <jpuelpan528@gmail.com>
2019-06-21 20:26:35 -0400

    Pause and restart game with r and space keys

--- snake.py
+++ snake.py
@@ -13,10 +13,18 @@ logging.basicConfig(
     datefmt='%d-%m-%Y %H:%M:%S'
 )
 
+DIR_UP = 'UP'
+DIR_LEFT = 'LEFT'
+DIR_DOWN = 'DOWN'
+DIR_RIGHT = 'RIGHT'
+
 class Snake(Thread):
     def __init__(self, screen, length=10):
         self.screen = screen
-        self.direction = 'UP'
+        self.direction = DIR_UP
+        self.previous_direction = DIR_UP
+        self.length = length
+        self.pause = False
 
         # Get screen dimensions
         height, width = screen.getmaxyx()
@@ -48,36 +56,65 @@ class Snake(Thread):
 
         while True:
             self.key = self.screen.getch()
-            # logging.debug('Key: %s', self.key)
+            logging.debug('Key: %s', self.key)
 
+            # Check for 'r' key and restart game
             if self.key == 114:
-                logging.debug('Restart!')
+                self.restart()
+
+            # Pause game with space key
+            if self.key == 32:
+                self.pause = not self.pause
+
+            self.previous_direction = self.direction
 
             if self.key == curses.KEY_UP:
-                self.direction = 'UP'
+                self.direction = DIR_UP
             elif self.key == curses.KEY_LEFT:
-                self.direction = 'LEFT'
+                self.direction = DIR_LEFT
             elif self.key == curses.KEY_DOWN:
-                self.direction = 'DOWN'
+                self.direction = DIR_DOWN
             elif self.key == curses.KEY_RIGHT:
-                self.direction = 'RIGHT'
+                self.direction = DIR_RIGHT
 
             logging.debug('Direction changed to: %s', self.direction)
 
+    def restart(self):
+        logging.debug('Restarting...')
+        self.direction = DIR_UP
+
+        self.body = []
+        self.rats = []
+
+        self.body.append((self.y, self.x))
+
+        for i in range(1, self.length):
+            self.body.append((self.y + i, self.x))
+
+        self.add_rat()
+        self.pause = False
+
     def move(self):
-        logging.debug('Snake length: %s, Rats: %s', len(self.body), len(self.rats))
+        # logging.debug('Snake length: %s, Rats: %s', len(self.body), len(self.rats))
+
+        if self.pause:
+            return
 
         head, *tail = self.body
         previous = head
 
-        if self.direction == 'UP':
+        if self.direction == DIR_UP:
             head = (head[0] - 1, head[1])
-        elif self.direction == 'LEFT':
+        elif self.direction == DIR_LEFT:
             head = (head[0], head[1] - 1)
-        elif self.direction == 'RIGHT':
+        elif self.direction == DIR_RIGHT:
             head = (head[0], head[1] + 1)
-        elif self.direction == 'DOWN':
+        elif self.direction == DIR_DOWN:
             head = (head[0] + 1, head[1])
+
+        if head == tail[0]:
+            self.direction = self.previous_direction
+            return
 
         self.body = [head]
 
@@ -104,13 +141,13 @@ class Snake(Thread):
             logging.debug('Yumy!')
             self.rats.remove(head)
 
-            if self.direction == 'UP':
+            if self.direction == DIR_UP:
                 self.body.append((last[0] - 1, last[1]))
-            if self.direction == 'DOWN':
+            if self.direction == DIR_DOWN:
                 self.body.append((last[0] + 1, last[1]))
-            if self.direction == 'LEFT':
+            if self.direction == DIR_LEFT:
                 self.body.append((last[0], last[1] - 1))
-            if self.direction == 'RIGHT':
+            if self.direction == DIR_RIGHT:
                 self.body.append((last[0], last[1] + 1))
 
             self.add_rat()
